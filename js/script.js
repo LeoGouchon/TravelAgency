6af95b0a8c4308d8a7c1d46b5ad259822350092a
Leo Gouchon <leo.gouchon@gmail.com>
2020-10-14 21:51:07 +0200

    paufinement du css de la page panier

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//
+  //
 //
 //
 //FONCTION POUR OUVRIR ET FERMER LE POP UP POUR SE LOGIN
@@ -187,3 +187,18 @@ for(var travel of travels){
     </div>
   </div>`;
 }
+
+//
+//
+//
+//FONCTION POUR LE BOUTON SCROLL
+//  ('#scrolltotop').click(function(){
+//  	('html,body').animate({scrollTop: 0}, 'slow');
+//  });
+//  (window).scroll(function(){
+//  	if((window).scrollTop()<500){
+//  		('#scrolltotop').fadeOut();
+//  	}else{
+//  		('#scrolltotop').fadeIn();
+//  	}
+//  });
