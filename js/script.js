d9aa6386ff05ea3eea2310e1d1053ee9460ce87c
Leo Gouchon <leo.gouchon@gmail.com>
2020-10-17 10:52:10 +0200

    fixe bug chemin image dans le js suppression testbtnscroll.js

--- js/script.js
+++ js/script.js
@@ -22,121 +22,121 @@ function closeForm() {
 //on créé une liste qui contient tout les voyages
 var travels =[
   {
-    'srcImgVille': '../data/destination/monzaVille.jpg',
-    'srcImgDrapeau': '../data/destination/italieDrapeau.jpg',
+    'srcImgVille': './data/destination/monzaVille.jpg',
+    'srcImgDrapeau': './data/destination/italieDrapeau.jpg',
     'ville':'Monza',
     'prix':'449',
     'pays':'Italie',
     'continent':'europe'
   },
   {
-    'srcImgVille':'../data/destination/spielbergVille.jpg',
-    'srcImgDrapeau':'../data/destination/autricheDrapeau.jpg',
+    'srcImgVille':'./data/destination/spielbergVille.jpg',
+    'srcImgDrapeau':'./data/destination/autricheDrapeau.jpg',
     'ville':'Spielberg',
     'prix':'',
     'continent':'europe'
   },
   {
-    'srcImgVille':'../data/destination/budapestVille.jpg',
-    'srcImgDrapeau':'../data/destination/hongrieDrapeau.jpg',
+    'srcImgVille':'./data/destination/budapestVille.jpg',
+    'srcImgDrapeau':'./data/destination/hongrieDrapeau.jpg',
     'ville':'Budapest',
     'prix':'',
     'continent':'europe'
   },
   {
-    'srcImgVille':'../data/destination/monacoVille.jpg',
-    'srcImgDrapeau':'../data/destination/monacoDrapeau.jpg',
+    'srcImgVille':'./data/destination/monacoVille.jpg',
+    'srcImgDrapeau':'./data/destination/monacoDrapeau.jpg',
     'ville':'Monaco',
     'prix':'',
     'continent':'europe'
   },
   {
-    'srcImgVille':'../data/destination/mexicoVille.jpg',
-    'srcImgDrapeau':'../data/destination/mexiqueDrapeau.jpg',
+    'srcImgVille':'./data/destination/mexicoVille.jpg',
+    'srcImgDrapeau':'./data/destination/mexiqueDrapeau.jpg',
     'ville':'Mexico',
     'prix':'',
     'continent':'ameriqueSud'
   },
   {
-    'srcImgVille':'../data/destination/barceloneVille.jpg',
-    'srcImgDrapeau':'../data/destination/espagneDrapeau.jpg',
+    'srcImgVille':'./data/destination/barceloneVille.jpg',
+    'srcImgDrapeau':'./data/destination/espagneDrapeau.jpg',
     'ville':'Barcelone',
     'prix':'',
     'continent':'europe'
   },
   {
-    'srcImgVille':'../data/destination/suzukaVille.jpg',
-    'srcImgDrapeau':'../data/destination/japonDrapeau.jpg',
+    'srcImgVille':'./data/destination/suzukaVille.jpg',
+    'srcImgDrapeau':'./data/destination/japonDrapeau.jpg',
     'ville':'Suzuka',
     'prix':'',
     'continent':'asie'
   },
   {
-    'srcImgVille':'../data/destination/istenbulVille.jpg',
-    'srcImgDrapeau':'../data/destination/turquieDrapeau.jpg',
+    'srcImgVille':'./data/destination/istenbulVille.jpg',
+    'srcImgDrapeau':'./data/destination/turquieDrapeau.jpg',
     'ville':'Istanbul',
     'prix':'',
     'continent':'asie'
   },
   {
-    'srcImgVille':'../data/destination/bahreïnVille.jpg',
-    'srcImgDrapeau':'../data/destination/BahreïnDrapeau.jpg',
+    'srcImgVille':'./data/destination/bahreïnVille.jpg',
+    'srcImgDrapeau':'./data/destination/BahreïnDrapeau.jpg',
     'ville':'Bahreïn',
     'prix':'',
     'continent':'asie'
   },
   {
-    'srcImgVille':'../data/destination/elroyVille.jpg',
-    'srcImgDrapeau':'../data/destination/etatsunisDrapeau.jpg',
+    'srcImgVille':'./data/destination/elroyVille.jpg',
+    'srcImgDrapeau':'./data/destination/etatsunisDrapeau.jpg',
     'ville':'Elroy',
     'prix':'',
     'continent':'ameriqueNord'
   },
   {
-    'srcImgVille':'../data/destination/castelletVille.jpg',
-    'srcImgDrapeau':'../data/destination/franceDrapeau.jpg',
+    'srcImgVille':'./data/destination/castelletVille.jpg',
+    'srcImgDrapeau':'./data/destination/franceDrapeau.jpg',
     'ville':'Castellet',
     'prix':'',
     'continent':'europe'
   },
   {
-    'srcImgVille':'../data/destination/sãopaoloVille.jpg',
-    'srcImgDrapeau':'../data/destination/bresilDrapeau.jpg',
+    'srcImgVille':'./data/destination/sãopaoloVille.jpg',
+    'srcImgDrapeau':'./data/destination/bresilDrapeau.jpg',
     'ville':'São Paulo',
     'prix':'',
     'continent':'ameriqueSud'
   },
   {
-    'srcImgVille':'../data/destination/kyalamiVille.jpg',
-    'srcImgDrapeau':'../data/destination/afriquesudDrapeau.jpg',
+    'srcImgVille':'./data/destination/kyalamiVille.jpg',
+    'srcImgDrapeau':'./data/destination/afriquesudDrapeau.jpg',
     'ville':'Kyalami',
     'prix':'',
     'continent':'afrique'
   },
   {
-    'srcImgVille':'../data/destination/shangaiVille.jpg',
-    'srcImgDrapeau':'../data/destination/chineDrapeau.jpg',
+    'srcImgVille':'./data/destination/shangaiVille.jpg',
+    'srcImgDrapeau':'./data/destination/chineDrapeau.jpg',
     'ville':'Shanghai',
     'prix':'',
     'continent':'asie'
   },
   {
-    'srcImgVille':'../data/destination/nurburgringVille.jpg',
-    'srcImgDrapeau':'../data/destination/allemagneDrapeau.jpg',
+    'srcImgVille':'./data/destination/nurburgringVille.jpg',
+    'srcImgDrapeau':'./data/destination/allemagneDrapeau.jpg',
     'ville':'nurburgring',
     'prix':'',
     'continent':'europe'
   },
   {
-    'srcImgVille':'../data/destination/singapourVille.jpg',
-    'srcImgDrapeau':'../data/destination/singapourDrapeau.jpg',
+    'srcImgVille':'./data/destination/singapourVille.jpg',
+    'srcImgDrapeau':'./data/destination/singapourDrapeau.jpg',
     'ville':'Singapour',
     'prix':'',
     'continent':'asie'
   },
   {
-    'srcImgVille':'../data/destination/melbourneVille.jpg',
-    'srcImgDrapeau':'../data/destination/australieDrapeau.jpg',
+    'srcImgVille':'./data/destination/melbourneVille.jpg',
+    'srcImgDrapeau':'./data/destination/australieDrapeau.jpg',
     'ville':'Merlbourne',
     'prix':'',
     'continent':'oceanie'
@@ -151,7 +151,7 @@ var travels =[
 
 ]
 
-var document = '../html/PagePrincipale.html';
+var document = '../index.html';
 for(var travel of travels){
   document.getElementsByClassName('sectionArticle')[0].innerHTML +=
   `<div class="voyage">
@@ -181,7 +181,7 @@ var mybutton = document.getElementById("topbutton");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    
+
     if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 20) {
       mybutton.style.display = "block";
     } else {
@@ -193,5 +193,3 @@ function scrollFunction() {
     document.body.scrollTop=0;
     document.documentElement.scrollTop=0;
 }
-
-
